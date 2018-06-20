654965be727a16c9dffdb553ae11d25d4c3c656c
bilalikram94 <bilalikram94@gmail.com>
2018-06-20 13:22:56 +0500

    Initial commit

---
+++ pages/home/login_page.py
@@ -0,0 +1,78 @@
+from base.selenium_drivers import SeleniumDriver
+import utilities.custom_logger as cl
+import logging
+import time
+
+
+class LoginPage(SeleniumDriver):
+    log = cl.customLogger(logging.DEBUG)
+
+    # Locators
+    _login_link = "Login"  # By Link-Text
+    _email_field = "user_email"  # By Name
+    _password_field = "user_password"  # By Name
+    _login_button = "commit"  # By Name
+    _login_success = "//input[@id='search-courses']"  # By Xpath
+    _failed_login = ".alert-danger"  # By CSS Selector
+    _user_settings = "/html//div[@id='navbar']//ul[@class='nav navbar-nav navbar-right']/li[@class='dropdown']/a"  # Xpath
+    _logout = "[href='\/sign_out']"  # By CSS Selector
+    _logout_success = ".btn-primary.text-center"  # By CSS Selector
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    def clickLoginLink(self):
+        self.elementClick(self._login_link, locatorType="link")
+
+    def enterEmail(self, email):
+        self.sendKeys(email, self._email_field)
+
+    def enterPassword(self, password):
+        self.sendKeys(password, self._password_field)
+
+    def clickLoginButton(self):
+        self.elementClick(self._login_button, locatorType="name")
+
+    def login(self, email="", password=""):
+        self.clickLoginLink()
+        # self.clearFields()
+        self.enterEmail(email)
+        self.enterPassword(password)
+        self.clickLoginButton()
+
+    def verifyLoginSuccessful(self):
+        result = self.isElementPresent(self._login_success, locatorType="xpath")
+        return result
+
+    # def clearFields(self):
+    #     emailField = self.getElement(locator=self._email_field)
+    #     emailField.clear()
+    #     passwordField = self.getElement(locator=self._password_field)
+    #     passwordField.clear()
+
+    def verifyLoginFailed(self):
+        result = self.isElementPresent(self._failed_login, locatorType="css")
+        return result
+
+    def verifyTitle(self):
+
+        if "Let's Kode It" in self.getTitle():
+            return True
+        else:
+            return False
+
+    def clickUserSettings(self):
+        self.elementClick(self._user_settings, locatorType="xpath")
+
+    def clickLogOut(self):
+        self.elementClick(self._logout, locatorType="css")
+
+    def verifySuccessfulLogout(self):
+        result = self.isElementPresent(self._logout_success, locatorType="css")
+        return result
+
+    def Logout(self):
+        self.clickUserSettings()
+        time.sleep(2)
+        self.clickLogOut()
---
+++ tests/home/login_test.py
@@ -0,0 +1,40 @@
+import time
+import pytest
+import unittest
+from utilities.teststatus import Status
+from pages.home.login_page import LoginPage
+
+
+@pytest.mark.usefixtures("oneTimeSetUp", "setUp")
+class LoginTests(unittest.TestCase):
+    @pytest.fixture(autouse=True)
+    def classSetup(self, oneTimeSetUp):
+        self.lp = LoginPage(self.driver)
+        self.ts = Status(self.driver)
+
+        """
+        Need to verify two verification points
+        1 fails, code will not go to the next verification point
+        If assert fails, it stops current test execution and moves to the next test method.
+        """
+
+    @pytest.mark.run(order=2)
+    def test_validLogin(self):
+        self.lp.login("test@email.com", "abcabc")
+        result1 = self.lp.verifyTitle()
+        self.ts.mark(result1, "Title is incorrect")
+        result2 = self.lp.verifyLoginSuccessful()
+        self.ts.markFinal("test_validLogin", result2, "Login was not successful")
+
+    @pytest.mark.run(order=1)
+    def test_invalidLogin(self):
+        time.sleep(3)
+        self.lp.login("test@email.com", "abcabcabc")
+        result = self.lp.verifyLoginFailed()
+        self.ts.markFinal("test_invalidLogin", result, "Login was successful")
+
+    @pytest.mark.run(order=3)
+    def test_validlogout(self):
+        self.lp.Logout()
+        result = self.lp.verifySuccessfulLogout()
+        self.ts.markFinal("test_validLogout", result, "Logout unsuccessful")
